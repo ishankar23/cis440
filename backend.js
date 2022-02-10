e754f9b351715bb50662b72e6938fbac8ea1aa9a
kmrinell <98300484+kmrinell@users.noreply.github.com>
2022-02-10 16:33:12 -0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
 let username = ""; 
 let password = ""; 
-
-const user = {un: username, pw: password}; 
-console.log(username); 
-console.log(password); 
-
+function validateUser(un, pw) {
+  const user = {un: username, pw: password}; 
+  console.log(username); 
+  console.log(password); 
+}
